b46835e947838b1d36dda401cfb4b0cf5f733397
Anton <anton.zeltser@gmail.com>
2025-03-23 16:02:28 +0100

    HW-13: add functionality delete all blogs

--- src/features/testing/api/testing.controller.ts
+++ src/features/testing/api/testing.controller.ts
@@ -1,20 +1,22 @@
 import {Controller, Delete, HttpCode, NotFoundException, Param, UseInterceptors} from "@nestjs/common";
 
-import {LoggingInterceptor} from "../../../common/interceptors/logging.interceptor";
 import {UsersService} from "../../users/application/user.service";
+import {BlogsService} from "../../blogs/blogs.service";
 
 @Controller('testing/all-data')
 export class TestingController {
     constructor(
-        private readonly usersService: UsersService
+        private readonly usersService: UsersService,
+        private readonly blogsService: BlogsService
     ) {
     }
 
     @Delete()
     @HttpCode(204)
     async delete() {
-       await this.usersService.deleteAll();
-                 console.log('deleted all elements')
+        await this.usersService.deleteAll();
+        await this.blogsService.deleteAll();
+        console.log('deleted all elements')
 
         /*        if (!deletingResult) {
                     throw new NotFoundException(`Error by deleting`);
